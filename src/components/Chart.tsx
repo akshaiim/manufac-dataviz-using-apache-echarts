f45fab1ba123a49a65ce84c6be29c092ec264bce
akshaiim <mishraakshaiii@gmail.com>
2021-06-29 14:45:26 +0530

    some code refactoring and bug fixes in bar chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -16,41 +16,63 @@ const Chart = ({ data}: any) => {
     Hue.push(val[11]);
   });
 
+
+  // getting values of alcohol and malic acids corresponding to each class
+
   const class1MalicAcid = malicAcid.slice(0, 59);
   const class2MalicAcid = malicAcid.slice(59, 130);
   const class3MalicAcid = malicAcid.slice(130);
 
-  // getting average of all 3 class of malic acids by adding and then dividing it by number of specimens belonging to that class and later pushing it into the array for plotting
+  const class1Alcohol = alcohol.slice(0, 59);
+  const class2Alcohol = alcohol.slice(59, 130);
+  const class3Alcohol = alcohol.slice(130);
+  
 
-  const class1average : (string|number)[] = [];
-  class1MalicAcid.forEach((item : any) => class1average.push(+item));
-  const Average1 = (class1average.reduce((a : any, b : any) => a + b, 0) / 59).toFixed(2);
+  // getting average of all 3 class of malic acids and alcohol by adding and then dividing it by number of specimens belonging to that class and later pushing it into the array for plotting
 
-  const class2average : (string|number)[] = [];
-  class2MalicAcid.forEach((item : any) => class2average.push(+item));
-  const Average2 = (class2average.reduce((a: any, b : any) => a + b, 0) / 71).toFixed(2);
+  const class1Malicaverage : (string|number)[] = [];
+  class1MalicAcid.forEach((item : any) => class1Malicaverage.push(+item));
+  const Averageclass1Malic = (class1Malicaverage.reduce((a : any, b : any) => a + b, 0) / 59).toFixed(2);
 
-  const class3average : (string|number)[] = [];
-  class3MalicAcid.forEach((item : any) => class3average.push(+item));
-  const Average3 = (class3average.reduce((a : any, b : any) => a + b, 0) / 48).toFixed(2);
+  const class2Malicaverage : (string|number)[] = [];
+  class2MalicAcid.forEach((item : any) => class2Malicaverage.push(+item));
+  const Averageclass2Malic = (class2Malicaverage.reduce((a: any, b : any) => a + b, 0) / 71).toFixed(2);
 
-  for (var i = 0; i <= 59; i++) {
-    malicAcid[i] = Average1;
-  }
-  for (var j = 60; j <= 130; j++) {
-    malicAcid[j] = Average2;
-  }
-  for (var k = 130; k < malicAcid.length; k++) {
-    malicAcid[k] = Average3;
-  }
+  const class3Malicaverage : (string|number)[] = [];
+  class3MalicAcid.forEach((item : any) => class3Malicaverage.push(+item));
+  const Averageclass3Malic = (class3Malicaverage.reduce((a : any, b : any) => a + b, 0) / 48).toFixed(2);
+
+
+  const class1Alcoholaverage : (string|number)[] = [];
+  class1Alcohol.forEach((item : any) => class1Alcoholaverage.push(+item));
+  const Averageclass1Alcohol = (class1Alcoholaverage.reduce((a : any, b : any) => a + b, 0) / 59).toFixed(2);
+
+  const class2Alcoholaverage : (string|number)[] = [];
+  class2Alcohol.forEach((item : any) => class2Alcoholaverage.push(+item));
+  const Averageclass2Alcohol = (class2Alcoholaverage.reduce((a: any, b : any) => a + b, 0) / 71).toFixed(2);
+
+  const class3Alcoholaverage : (string|number)[] = [];
+  class3Alcohol.forEach((item : any) => class3Alcoholaverage.push(+item));
+  const Averageclass3Alcohol = (class3Alcoholaverage.reduce((a : any, b : any) => a + b, 0) / 48).toFixed(2);
+
+  // for (var i = 0; i <= 59; i++) {
+  //   malicAcid[i] = Average1;
+  // }
+  // for (var j = 60; j <= 130; j++) {
+  //   malicAcid[j] = Average2;
+  // }
+  // for (var k = 130; k < malicAcid.length; k++) {
+  //   malicAcid[k] = Average3;
+  // }
 
   // data to be passed to the echarts components for bar chart
+
 
   const barChartOptions = {
     xAxis: {
       type: "category",
       name: "alcohol",
-      data: alcohol,
+      data: [Averageclass1Alcohol,Averageclass2Alcohol,Averageclass3Alcohol],
     },
     yAxis: {
       type: "value",
@@ -58,7 +80,7 @@ const Chart = ({ data}: any) => {
     },
     series: [
       {
-        data: malicAcid,
+        data: [Averageclass1Malic,Averageclass2Malic,Averageclass3Malic],
         type: "bar",
         showBackground: true,
         smooth: true,
